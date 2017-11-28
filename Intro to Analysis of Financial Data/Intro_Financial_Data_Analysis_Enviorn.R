3b8230753fc1f6c7ddf9094ee8e01c486af9c8d7
Frank-Jing <jingwx36@gmail.com>
2017-11-27 22:34:44 -0800

    Finish Tsay R Ch1

---
+++ Intro to Analysis of Financial Data/Intro_Financial_Data_Analysis_Enviorn.R
@@ -0,0 +1,6 @@
+## This is a R environment setting document
+## The main role it plays is to :
+### 1. Check working space path
+### 2. Install and use packages used in the book
+### 3. List each chapter's subjects
+
